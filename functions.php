24d7f8d005c2652a338948213da0ca30dbb1755a
Alexander Agnarson <alexander.agnarson@gmail.com>
2021-07-23 18:46:19 +0200

    Disable support for widgets block editor, regen lang

--- functions.php
+++ functions.php
@@ -69,6 +69,9 @@ if ( ! function_exists( 'motioner_setup' ) ) {
 		// Enable support for selective refresh of widgets in customizer
 		add_theme_support( 'customize-selective-refresh-widgets' );
 		
+		// Disable support for widgets block editor
+		remove_theme_support( 'widgets-block-editor' );
+		
 		// Thumbnail sizes
 		add_image_size( 'motioner-small', 460, 460, true );
 		add_image_size( 'motioner-medium', 520, 293, true );
